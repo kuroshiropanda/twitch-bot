d80dcfc5b8ab2a3055a5963bee539d90467573d0
AJ Lunas <kuroshiropanda@outlook.com>
2020-09-18 12:42:17 +0800

    Update code refactor

--- src/services/chat/chat.ts
+++ src/services/chat/chat.ts
@@ -1,47 +1,146 @@
-import ChatClient from 'twitch-chat-client'
+import { ApiClient } from 'twitch'
+import { RefreshableAuthProvider, StaticAuthProvider } from 'twitch-auth'
+import { ChatClient } from 'twitch-chat-client'
 import say from 'say'
-import { User } from '../mongo/mongo'
-import { auth } from './auth'
-import { obsControl } from '../obs/obs'
+import Server from 'socket.io'
+import { twitch } from '../../config/twitch'
+import { Bot, Log } from '../mongo/mongo'
+// import { obsControl } from '../obs/obs'
+import Twitch from '../twitch/twitch'
 
-const getChannels = async () => {
-    return await User.readAll()
-}
-
-const chatOptions: object = {
-    webSocket: true,
-    requestMembershipEvents: true,
-    channels: getChannels
-}
-
-const chat = ChatClient.forTwitchClient(auth, chatOptions)
-
-chat.connect().then(() => {
-    console.log('bot connected successfully')
-}).catch((e) => {
-    console.log(e)
-})
-
-chat.onRaid((channel, user, raidInfo, msg) => {
-    if (channel === '#kuroshiropanda') {
-        obsControl.tbc()
-    }
-})
-
-chat.onPrivmsg((channel, user, message, msg) => {
-    console.log(`${user} : ${message}`)
-
-    if (msg.tags.get('msg-id') === 'highlighted-message') {
-        say.speak(message)
-    }
-
-    const command = message.trim()
-
-    if (command === '!ss') {
-        if (user === 'kuroshiropanda') {
-            say.stop()
+const Chat = async () => {
+    const ClientOnRefresh = async ({ accessToken, refreshToken, expiryDate }) => {
+        const newTokenData = {
+            twitchId: '74955654',
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            expiry: expiryDate === null ? null : expiryDate.getTime()
         }
-    } else if (command === '!dance') {
-        chat.say(channel, 'for each sub or 500 bits cheer there would be a random chance for a dance')
+
+        Bot.update(newTokenData)
     }
-})
+
+    const bot: any = await Bot.read({ twitchId: '74955654' })
+
+    const auth = new RefreshableAuthProvider(
+        new StaticAuthProvider(twitch.clientId, bot.accessToken), {
+            clientSecret: twitch.clientSecret,
+            refreshToken: bot.refreshToken,
+            expiry: bot.expiry === null ? null : new Date(bot.expiry),
+            onRefresh: ClientOnRefresh
+        })
+
+    interface chatOptsObj {
+        webSocket: boolean
+        channels: string[]
+    }
+
+    const chatOptions: chatOptsObj = {
+        webSocket: true,
+        channels: await Twitch.getUsers()
+    }
+
+    const socket = new Server(8000)
+    socket.on('connection', (socket) => {
+        console.log('socketio: connected')
+    })
+    const chat = new ChatClient(auth, chatOptions)
+    const api = new ApiClient({ authProvider: auth })
+
+    try {
+        await chat.connect()
+        console.log('Chat Bot: connected', chatOptions.channels)
+
+    } catch (err) {
+        console.log(err)
+    }
+
+    chat.onRaid(async (channel, user, raidInfo, msg) => {
+        if (channel === '#kuroshiropanda') {
+            // obsControl.tbc()
+        }
+        
+        const userId = await api.kraken.users.getUserByName(user)
+        socket.emit('ClipShoutout', await Twitch.getRandomClip(userId.id))
+        chat.say(channel, `Shoutout to ${user} for raiding us with ${raidInfo.viewerCount} people`)
+    })
+
+    chat.onHosted(async (channel, user, auto, viewers) => {
+        console.log({ user, auto, viewers })
+        if (!auto) {
+            const userId = await api.kraken.users.getUserByName(user)
+            socket.emit('ClipShoutout', await Twitch.getRandomClip(userId.id))
+            chat.say(channel, `Shoutout to ${user} for hosting us with ${viewers} people`)
+        }
+    })
+
+    chat.onMessage(async (channel, user, message, msg) => {
+        //  || user === channel.substr(1, channel.length)
+        if (user === chat.currentNick) return
+        
+        const command = message.toLowerCase().trim()
+
+        const bots = /buy follower/gi
+        const mama = /\bmama\b/gi
+        const hatdog = /\bha\?\b/gi
+
+        const log = new Log({ 
+            channel: channel,
+            user: user,
+            msg: message,
+            date: new Date()
+        })
+        log.save()
+
+        if (bots.test(command)) {
+            chat.ban(channel, user, 'Spam bot')
+            return
+        }
+
+        if (user === 'Nightbot' || 'u2san_' || 'kuroshiropanda_') {
+            socket.emit('NicoNicoChat', { id: msg.tags.get('id'), emotes: msg.parseEmotes(), message })
+        }
+
+        if (msg.tags.get('msg-id') === 'highlighted-message') {
+            say.speak(message)
+        }
+
+        if (command.split(' ')[0] === '!so') {
+            const username = command.split(' ')[1].replace('@', '')
+            const userId = await api.kraken.users.getUserByName(username)
+            socket.emit('ClipShoutout', await Twitch.getRandomClip(userId.id))
+        }
+
+        if (command === '!ss') {
+            if (user === 'kuroshiropanda') {
+                say.stop()
+            }
+        }
+
+        if (command === '!vanish') {
+            chat.timeout(channel, user, 1, `${user} used vanish`)
+        }
+        
+        if (command === '!dance') {
+            chat.say(channel, 'for each sub/500 bits/$5 donation there would be a random chance for a dance')
+        }
+
+        if (mama.test(command)) {
+            chat.say(channel, `@${user} mama mo`)
+        }
+
+        if (hatdog.test(command)) {
+            chat.say(channel, `@${user} hatdog`)
+        }
+    })
+
+    chat.onMessageRatelimit((channel, message) => {
+        console.log('rate limit reached', { channel, message })
+    })
+
+    chat.onTimeout((channel, user, duration) => {
+        chat.say(channel, `${user} was timed out for ${duration}`)
+    })
+}
+
+export default Chat
--- src/services/streamlabs/streamlabs.ts
+++ src/services/streamlabs/streamlabs.ts
@@ -1,37 +1,82 @@
-'use strict';
+import axios from 'axios'
+import io from 'socket.io-client'
+import { promises as fs } from 'fs'
+import { streamlabs } from '../../config/streamlabs'
 
-const axios = require('axios').default;
-const fs = require('fs');
+export default class Streamlabs {
 
-const SLData = JSON.parse(fs.readFileSync('./streamlabs.json', { encoding: 'utf8' }));
+    private socket: string
 
-const streamlabs = {
-    credits: () => {
-        axios.post('https://streamlabs.com/api/v1.0/credits/roll', {
-            access_token: SLData.access_token
-        }).catch((e) => {
-            console.error(e);
-        });
-    },
+    constructor(socket: string) {
+        this.socket = socket
+    }
 
-    alerts: () => {
-        axios.post('https://streamlabs.com/api/v1.0/alerts', {
-            access_token: SLData.access_token,
-            type: 'donation',
-            message: 'Test alert',
-            duration: '8000'
-        }).catch((e) => {
-            console.error(e);
-        });
-    },
+    async init() {
+        return io(`https://sockets.streamlabs.com`, {
+            transports: ['websocket'],
+            query: {
+                token: await Streamlabs.token()
+            }
+        })
+    }
 
-    testAlert: () => {
-        axios.post('https://streamlabs.com/api/v1.0/alerts/send_test_alert', {
-            access_token: SLData.access_token,
-            type: 'donation',
-            platform: 'twitch'
-        });
+    private static async token() {
+        const data = JSON.parse(await fs.readFile('./streamlabs.json', { encoding: 'utf8' }))
+        return data.access_token
+    }
+
+    static async getToken(code: any) {
+        try {
+            const token = await axios({
+                method: 'POST',
+                url: 'https://streamlabs.com/api/v1.0/token',
+                data: {
+                    client_id: streamlabs.clientId,
+                    client_secret: streamlabs.clientSecret,
+                    code: code,
+                    grant_type: 'authorization_code',
+                    redirect_uri: streamlabs.redirectURI
+                }
+            })
+
+            return token.data
+        } catch (err) {
+            return err
+        }
+    }
+
+    static async credits() {
+        try {
+            await axios.post('https://streamlabs.com/api/v1.0/credits/roll', {
+                access_token: await this.token()
+            })
+        } catch (err) {
+            throw err.data.message
+        }
+    }
+
+    static async alerts() {
+        try {
+            await axios.post('https://streamlabs.com/api/v1.0/alerts', {
+                access_token: await this.token(),
+                type: 'donation',
+                message: 'Test alert',
+                duration: '8000'
+            })
+        } catch (err) {
+            throw err.data.message
+        }
+    }
+
+    static async testAlert() {
+        try {
+            axios.post('https://streamlabs.com/api/v1.0/alerts/send_test_alert', {
+                access_token: await this.token(),
+                type: 'donation',
+                platform: 'twitch'
+            })
+        } catch (err) {
+            throw err.data.message
+        }
     }
 }
-
-module.exports = streamlabs;
