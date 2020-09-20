ae9c4c4c20d0bea9fe4a50bf8337108b114a6865
AJ Lunas <kuroshiropanda@outlook.com>
2020-09-21 03:07:44 +0800

    Update niconico chat overlay use twitch emotes

--- resources/views/niconico/script.js
+++ resources/views/niconico/script.js
@@ -3,62 +3,46 @@ socket.connect()
 
 let messagesArray = []
 let maxMovementNums = 3
-// let movementNum = 0
 
 const messagesDiv = document.getElementById('messages')
 
 socket.on('NicoNicoChat', (data) => {
   console.log(data)
-  // const context = eventData.context
-  // const fontColor = eventData.color
   let emotes = data.emotes
   let text = data.message
+  let user = data.user
 
   if (text.startsWith('!')) return
 
-  // Ignore messages that contain any possible HTML tags
   const regex = /<.*>/g
   if (text.search(regex) >= 0) return
 
+  // if (user.toLowerCase() == 'nightbot' || 'u2san_' || 'kuroshiropanda_') return
+
   const msgDiv = document.createElement('div')
-  // const nameDiv = document.createElement('div')
-  const msgContentDiv = document.createElement('div')
+  const msgContentDiv = document.createElement('span')
 
   let movementNum = 0
-  if (text.length < 25) {
-    movementNum = 0
-  } else if (text.length > 26) {
-    movementNum = 1
-  } else if (text.length >= 50) {
-    movementNum = 2
-  } else {
-    movementNum = 3
+
+  msg = ''
+  for (const em of emotes) {
+    if (em.id) {
+      msg += `<span><img class="emotes" src="https://static-cdn.jtvnw.net/emoticons/v1/${em.id}/2.0" /></span>`
+    } else {
+      msg += `${em.text}`
+    }
   }
 
   msgDiv.id = data.id
   msgDiv.className += 'message'
   msgDiv.className += ` messageMovement${movementNum}`
 
-  // nameDiv.className += 'messageUsername'
   msgContentDiv.className += 'messageContent'
 
-  // Add emote images to message, if emotes exist
-  // if (context.emotes !== null) {
-  //   text = insertEmotesIntoMessage(context.emotes, text)
-  // }
-
-  // Color the username, if the color exists
-  // if (fontColor !== undefined && fontColor != null) {
-  //   nameDiv.innerHTML = `<font color="${fontColor}">${context['display-name']}</font>`
-  //   msgContentDiv.innerHTML = `: ${text}`
-  // } else {
-  //   msgContentDiv.innerHTML = `${context['display-name']}: ${text}`
-  // }
-  msgContentDiv.innerHTML = text
+  msgContentDiv.innerHTML = msg
 
   msgDiv.style.top = Math.random() * 400
 
-  // msgDiv.appendChild(nameDiv)
   msgDiv.appendChild(msgContentDiv)
   messagesDiv.appendChild(msgDiv)
 
@@ -68,43 +52,3 @@ socket.on('NicoNicoChat', (data) => {
     document.getElementById(`${msgDiv.id}`).outerHTML = ''
   }, 20000)
 })
-
-function insertEmotesIntoMessage(emotesObject, text) {
-  let emotes = []
-
-  // Extract all emotes from emotes object,
-  // and store their information
-  for (let [key, value] of Object.entries(emotesObject)) {
-    for (let v of value) {
-      const indexes = v.split('-')
-      emotes.push({
-        emote: key,
-        url: `https://static-cdn.jtvnw.net/emoticons/v1/${key}/1.0`,
-        startIndex: parseInt(indexes[0]),
-        endIndex: parseInt(indexes[1])
-      })
-    }
-  }
-
-  // Sort emotes from last to first
-  emotes.sort((a, b) => {
-    if (a.startIndex > b.startIndex) {
-      return -1
-    }
-    if (a.startIndex < b.startIndex) {
-      return 1
-    }
-    return 0
-  })
-
-  // Loop through the emotes and insert the images from back to front in the message string
-  let textAndEmotes = '' + text
-  for (emote of emotes) {
-    textAndEmotes =
-      textAndEmotes.substring(0, emote.startIndex) +
-      `<span><img src=${emote.url}></img></span>` +
-      textAndEmotes.substring(emote.endIndex + 1)
-  }
-
-  return textAndEmotes
-}
