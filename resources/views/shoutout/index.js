4202dc779b72ec5b064f30d31dd1fa99181135fb
AJ Lunas <kuroshiropanda@outlook.com>
2020-09-21 03:08:12 +0800

    Update shoutout overlay to use queueing

---
+++ resources/views/shoutout/index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const socket = io('http://localhost:8000');
+socket.connect();
+
+socket.on('connect', () => console.log('socketio: connected'));
+
+const shoutout = document.getElementById('shoutout');
+const player = document.getElementById('clip');
+const title = document.getElementById('username');
+player.volume = 0.5;
+player.autoplay = true;
+
+let clips = [];
+
+socket.on('ClipShoutout', (data) => {
+  console.log(data);
+  shoutout.style.display = 'block';
+  const num = randNum(data.clips.length - 1);
+  const user = `twitch.tv/${data.user}`;
+  if (!player.paused && !player.ended) {
+    clips.push({
+      user: user,
+      clip: data.clips[num]
+    });
+  } else {
+    title.innerHTML = user;
+    player.src = data.clips[num];
+    player.play();
+  }
+});
+
+function randNum(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function checkQueue() {
+  if (clips.length > 0) {
+    const queue = clips.shift();
+    title.innerHTML = queue.user;
+    player.src = queue.clip;
+    player.play();
+  } else {
+    removeVideo();
+  }
+}
+
+function removeVideo() {
+  shoutout.style.display = 'none';
+  title.innerHTML = '';
+  player.src = '';
+}
