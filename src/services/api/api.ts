244fa2802f9d3e1b2c1c5b1b4c755e3a0dd8a062
AJ Lunas <kuroshiropanda@outlook.com>
2020-09-30 05:18:47 +0800

    Add api module

---
+++ src/services/api/api.ts
@@ -0,0 +1,8 @@
+import { ApiClient } from 'twitch'
+import { ClientCredentialsAuthProvider } from 'twitch-auth'
+import { twitch } from '../../config/twitch'
+
+const auth = new ClientCredentialsAuthProvider(twitch.clientId, twitch.clientSecret);
+const api = new ApiClient({ authProvider: auth, initialScopes: twitch.scopes })
+
+export { api }
