5029a05ea3f2fb5b84f18a7ef85fb386d5865a5b
AJ Lunas <kuroshiropanda@outlook.com>
2020-10-02 03:57:14 +0800

    Add bot scopes to twitch config

--- src/config/twitch.ts
+++ src/config/twitch.ts
@@ -1,43 +1,55 @@
 declare let process: {
-    env: {
-        CHANNEL: string,
-        TWITCH_CLIENT_ID: string,
-        TWITCH_CLIENT_SECRET: string,
-        TWITCH_CALLBACK_URI: string
-    }
+  env: {
+    CHANNEL: string,
+    TWITCH_CLIENT_ID: string,
+    TWITCH_CLIENT_SECRET: string,
+    TWITCH_CALLBACK_URI: string
+  }
 }
 
 interface clientObj {
-    channel: string
-    clientId: string
-    clientSecret: string
-    redirectURI: string
-    scopes: Array<string>
+  channel: string
+  clientId: string
+  clientSecret: string
+  redirectURI: string
+  scopes: string[]
+  botScopes: string[]
 }
 
 export const twitch: clientObj = {
-    channel: process.env.CHANNEL,
-    clientId: process.env.TWITCH_CLIENT_ID,
-    clientSecret: process.env.TWITCH_CLIENT_SECRET,
-    redirectURI: process.env.TWITCH_CALLBACK_URI,
-    scopes: [
-        'bits:read',
-        'channel:moderate',
-        'channel:edit:commercial',
-        'channel:read:hype_train',
-        'channel:read:subscriptions',
-        'channel:read:redemptions',
-        'clips:edit',
-        'chat:edit',
-        'chat:read',
-        'user:edit',
-        'user:edit:broadcast',
-        'user:read:broadcast',
-        'user:read:email',
-        'channel_commercial',
-        'channel_editor',
-        'channel_feed_edit',
-        'channel_feed_read',
-        'channel_subscriptions'
-    ]
+  channel: process.env.CHANNEL,
+  clientId: process.env.TWITCH_CLIENT_ID,
+  clientSecret: process.env.TWITCH_CLIENT_SECRET,
+  redirectURI: process.env.TWITCH_CALLBACK_URI,
+  scopes: [
+    'bits:read',
+    'channel:moderate',
+    'channel:edit:commercial',
+    'channel:read:hype_train',
+    'channel:read:subscriptions',
+    'channel:read:redemptions',
+    'clips:edit',
+    'chat:edit',
+    'chat:read',
+    'user:edit',
+    'user:edit:broadcast',
+    'user:read:broadcast',
+    'user:read:email',
+    'channel_commercial',
+    'channel_editor',
+    'channel_feed_edit',
+    'channel_feed_read',
+    'channel_subscriptions'
+  ],
+  botScopes: [
+    'channel:moderate',
+    'chat:edit',
+    'chat:read',
+    'whispers:read',
+    'whispers:edit',
+    'channel:edit:commercial',
+    'clips:edit',
+    'channel_commercial',
+    'channel_editor',
+  ]
 }
